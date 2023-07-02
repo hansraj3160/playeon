076efa8b4821981d9c2ea6edac017a5f6a809a9f
hansraj <hansraj.bcsf18@iba-suk.edu.pk>
2023-07-02 13:07:34 +0500

    done

--- lib/dashboard/profile.dart
+++ lib/dashboard/profile.dart
@@ -289,53 +289,53 @@ class _ProfileState extends State<Profile> {
                           SizedBox(
                             height: size.height * 0.01,
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              MyButton(
-                                btnHeight: size.height * 0.055,
-                                btnWidth: size.width * 0.70,
-                                btnTxt: "Feedback",
-                                btnColor: Colors.white,
-                                btnRadius: 25,
-                                borderColor: textColor1,
-                                txtColor: textColor5,
-                                fontSize: 15,
-                                onTap: () async {
-                                  await Navigator.push(
-                                      context,
-                                      SwipeLeftAnimationRoute(
-                                          milliseconds: 200,
-                                          widget: FeedbackScreen()));
-                                },
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: size.height * 0.01,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              MyButton(
-                                btnHeight: size.height * 0.055,
-                                btnWidth: size.width * 0.70,
-                                btnTxt: "Movie Request",
-                                btnColor: Colors.white,
-                                btnRadius: 25,
-                                borderColor: textColor1,
-                                txtColor: textColor5,
-                                fontSize: 15,
-                                onTap: () async {
-                                  await Navigator.push(
-                                      context,
-                                      SwipeLeftAnimationRoute(
-                                          milliseconds: 200,
-                                          widget: RequestMovieScreen()));
-                                },
-                              ),
-                            ],
-                          )
+                          // Row(
+                          //   mainAxisAlignment: MainAxisAlignment.center,
+                          //   children: [
+                          //     MyButton(
+                          //       btnHeight: size.height * 0.055,
+                          //       btnWidth: size.width * 0.70,
+                          //       btnTxt: "Feedback",
+                          //       btnColor: Colors.white,
+                          //       btnRadius: 25,
+                          //       borderColor: textColor1,
+                          //       txtColor: textColor5,
+                          //       fontSize: 15,
+                          //       onTap: () async {
+                          //         await Navigator.push(
+                          //             context,
+                          //             SwipeLeftAnimationRoute(
+                          //                 milliseconds: 200,
+                          //                 widget: FeedbackScreen()));
+                          //       },
+                          //     ),
+                          //   ],
+                          // ),
+                          // SizedBox(
+                          //   height: size.height * 0.01,
+                          // ),
+                          // Row(
+                          //   mainAxisAlignment: MainAxisAlignment.center,
+                          //   children: [
+                          //     MyButton(
+                          //       btnHeight: size.height * 0.055,
+                          //       btnWidth: size.width * 0.70,
+                          //       btnTxt: "Movie Request",
+                          //       btnColor: Colors.white,
+                          //       btnRadius: 25,
+                          //       borderColor: textColor1,
+                          //       txtColor: textColor5,
+                          //       fontSize: 15,
+                          //       onTap: () async {
+                          //         await Navigator.push(
+                          //             context,
+                          //             SwipeLeftAnimationRoute(
+                          //                 milliseconds: 200,
+                          //                 widget: RequestMovieScreen()));
+                          //       },
+                          //     ),
+                          //   ],
+                          // )
                           // Container(
                           //   width: size.width,
                           //   height: size.height * 0.6,
